7bbcbe88355393c439a9a6b38a2070f013676412
谢潇 <softwarexiexiao@gmail.com>
2015-07-13 19:56:49 +0800

    。。

--- circle packing/circlePacking.js
+++ circle packing/circlePacking.js
@@ -1,10 +1,3 @@
-Array.prototype.first = function() {
-	return this[0];
-}
-Array.prototype.end = function() {
-	return this[this.length - 1];
-}
-
 function Bubble(color, r) {
 	this.color = color;
 	this.r = r;
@@ -27,6 +20,10 @@ function isOverlap(circle, queue) {
 }
 
 function getPosition(circle1, circle2) {
+	var getAngle = function(cur, center) {
+		var angle = Math.atan2(-cur.y + center.y, cur.x - center.x);
+		return angle;
+	}
 	var r1 = circle1.r + 1,
 		r2 = circle2.r + 1,
 		d = Math.sqrt(Math.pow(circle1.x - circle2.x, 2) + Math.pow(circle1.y - circle2.y, 2));
@@ -53,28 +50,13 @@ function getPosition(circle1, circle2) {
 	};
 }
 
-function getAngle(cur, center) {
-	var angle = Math.atan2(-cur.y + center.y, cur.x - center.x);
-	return angle;
-}
-
-function main() {
-	var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-	document.getElementsByTagName("body")[0].appendChild(svg);
-	var w = 960,
-		h = 960;
-	svg.style.width = w;
-	svg.style.height = h;
-	var data = [];
-	for (var i = 0; i < 200; i++) {
-		data.push(new Bubble(getColor(), Math.floor(Math.random() * 35) + 5));
-	}
+function layout(data) {
 	data.sort(function(a, b) {
 		return a.r - b.r;
 	});
 	var center = {
-		x: w / 2,
-		y: h / 2
+		x: 0,
+		y: 0
 	};
 	var queue = [];
 	var count = 0;
@@ -82,24 +64,12 @@ function main() {
 		if (i == 0) {
 			data[i].x = center.x;
 			data[i].y = center.y;
-			var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-			circle.setAttribute('cx', data[i].x);
-			circle.setAttribute('cy', data[i].y);
-			circle.setAttribute('r', data[i].r);
-			circle.setAttribute('fill', data[i].color);
-			svg.appendChild(circle);
 			queue.push(data[i]);
 		} else {
 			while (1) {
 				if (i == 1) {
 					data[i].x = queue[count].x + data[i].r + queue[count].r;
 					data[i].y = queue[count].y;
-					var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-					circle.setAttribute('cx', data[i].x);
-					circle.setAttribute('cy', data[i].y);
-					circle.setAttribute('r', data[i].r);
-					circle.setAttribute('fill', data[i].color);
-					svg.appendChild(circle);
 					queue.push(data[i]);
 					break;
 				}
@@ -126,17 +96,49 @@ function main() {
 				if (isOverlap(data[i], queue)) {
 					count++;
 				} else {
-					var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-					circle.setAttribute('cx', data[i].x);
-					circle.setAttribute('cy', data[i].y);
-					circle.setAttribute('r', data[i].r);
-					circle.setAttribute('fill', data[i].color);
-					svg.appendChild(circle);
 					queue.push(data[i]);
 					break;
 				}
 			}
 		}
+	}
+}
+
+function transition(position, data) {
+	if (!data || !data.length || data.length == 0)
+		return false;
+	var dx = position.x - data[0].x,
+		dy = position.y - data[0].y;
+	for (var i = 0; i < data.length; i++) {
+		data[i].x += dx;
+		data[i].y += dy;
+	}
+	return true;
+}
+
+function main() {
+	var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+	document.getElementsByTagName("body")[0].appendChild(svg);
+	var w = 960,
+		h = 960;
+	svg.style.width = w;
+	svg.style.height = h;
+	var data = [];
+	for (var i = 0; i < 6; i++) {
+		data.push(new Bubble(getColor(), Math.floor(Math.random() * 35) + 5));
+	}
+	layout(data);
+	transition({
+		x: w / 3,
+		y: h / 2
+	}, data);
+	for (var i = 0; i < data.length; i++) {
+		var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+		circle.setAttribute('cx', data[i].x);
+		circle.setAttribute('cy', data[i].y);
+		circle.setAttribute('r', data[i].r);
+		circle.setAttribute('fill', data[i].color);
+		svg.appendChild(circle);
 	}
 }
 window.onload = main;
